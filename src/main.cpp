351da1c3a908ddc069e927d6dca36a157420907b
FRBF Studios <tfroempp@outlook.de>
2026-08-15 16:37:46 +0200

    Committed everything for vacation

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,8 @@
-#include <vulkan/vulkan.h>
+#if defined(__INTELLISENSE__) || !defined(USE_CPP20_MODULES)
+#include <vulkan/vulkan_raii.hpp>
+#else
+import vulkan_hpp;
+#endif
 
 #include <iostream>
 #include <stdexcept>
